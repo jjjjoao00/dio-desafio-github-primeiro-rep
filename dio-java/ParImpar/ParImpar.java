a8ff1628c01e14f5b3ae90a188f3f1a85c92f9a5
jjjjoao00 <108374205+jjjjoao00@users.noreply.github.com>
2023-01-12 15:12:41 +0000

    add ParImpar

---
+++ dio-java/ParImpar/ParImpar.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    int quantidadeNumero;
+    int contador = 0;
+    int numero;
+    Scanner scan = new Scanner(System.in);
+    int quantidadePares = 0;
+    int quantidadeImpares = 0;
+
+    
+    //informa quantidade de numeros a serem inseridos
+    System.out.println("Quantidade de Numeros: ");
+    quantidadeNumero = scan.nextInt();
+
+    do{
+      System.out.println("Numero: ");
+      numero = scan.nextInt();
+
+     //verificação de pares e impares
+      if (numero % 2 ==0) {
+        quantidadePares ++;
+      }else{
+        quantidadeImpares ++;
+      }
+    
+      //contagem de numeros pares e numeros impares
+      contador++;
+
+        
+      
+    }while(contador < quantidadeNumero);
+
+    System.out.println("Quantidade Par: "+quantidadePares + " Quantidade Ímpar: " + quantidadeImpares);
+
+  
+
+    
+  
+
+    
+  }
+}
